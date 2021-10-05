e0a5dfd72f5a1f9f1197fd1481b2b1ba943842e2
JVictorGalvao <joaogalvao@eng.ci.ufpb.br>
2021-10-04 23:30:32 -0300

    feat: Adicionando tela de registro

--- screens/RegisterScreen.tsx
+++ screens/RegisterScreen.tsx
@@ -1,12 +1,37 @@
-import React from 'react';
-import { Appbar, Button, TextInput } from 'react-native-paper';
+import React, { useState } from 'react';
+import { Appbar, Button, HelperText, TextInput } from 'react-native-paper';
 import { ScreenContainer } from '../components/ScreenContainer';
 import { Separator } from '../components/Separator';
+import api from '../service/api';
 import { RootStackScreenProps } from '../types';
 
 export default function RegisterScreen({
   navigation,
 }: RootStackScreenProps<'Registro'>) {
+  const [nome, setNome] = useState('');
+  const [dataNasc, setDataNasc] = useState('');
+  const [email, setEmail] = useState('');
+  const [senha, setSenha] = useState('');
+  const [confirmacao, setConfirmacao] = useState('');
+  const [erro, setErro] = useState(false);
+  const hasErrors = () => senha !== confirmacao;
+  const registrar = () => {
+    if (hasErrors()) {
+      setErro(true);
+    } else {
+      setErro(false);
+      api
+        .post('/users', {
+          nome: nome,
+          email: email,
+          password: senha,
+          data_nascimento: dataNasc,
+        })
+        .then((response) => console.log(response.data))
+        .then(() => navigation.navigate('Login'));
+    }
+  };
+
   return (
     <>
       <Appbar.Header statusBarHeight={0}>
@@ -19,22 +44,55 @@ export default function RegisterScreen({
       </Appbar.Header>
       <ScreenContainer>
         <Separator vertical size={64} />
-        <TextInput label="Nome completo" mode="outlined" />
+        <TextInput
+          label="Nome completo"
+          mode="outlined"
+          value={nome}
+          onChangeText={(nome) => setNome(nome)}
+        />
         <Separator vertical size={16} />
-        <TextInput label="Data de nascimento" mode="outlined" />
+        <TextInput
+          label="Data de nascimento"
+          mode="outlined"
+          keyboardType="numeric"
+          value={dataNasc}
+          onChangeText={(dataNasc) => setDataNasc(dataNasc)}
+        />
+
         <Separator vertical size={16} />
-        <TextInput label="Email" mode="outlined" />
+        <TextInput
+          label="Email"
+          mode="outlined"
+          value={email}
+          onChangeText={(email) => setEmail(email)}
+        />
         <Separator vertical size={16} />
-        <TextInput label="Senha" secureTextEntry mode="outlined" />
+        <TextInput
+          label="Senha"
+          secureTextEntry
+          mode="outlined"
+          value={senha}
+          onChangeText={(senha) => setSenha(senha)}
+        />
         <Separator vertical size={16} />
 
         <TextInput
           label="Confirmação de senha"
           mode="outlined"
           secureTextEntry
+          value={confirmacao}
+          onChangeText={(confirmacao) => setConfirmacao(confirmacao)}
         />
+        <HelperText type="error" visible={erro}>
+          As senhas não coincidem
+        </HelperText>
         <Separator vertical size={64} />
-        <Button mode="contained" onPress={() => console.log('Pressed')}>
+        <Button
+          mode="contained"
+          onPress={() => {
+            registrar();
+          }}
+        >
           Inscrever-se
         </Button>
       </ScreenContainer>
